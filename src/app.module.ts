5a47c54ec55cecbef7f6b1ff3a0bad381850035f
Patricia Lo <patricia.lo@xyonhealth.com>
2024-10-15 16:39:09 -0700

    feat: add swagger and modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MonstersController } from './monsters/monsters.controller';
+import { ItemsController } from './items/items.controller';
+import { WeaponsController } from './weapons/weapons.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [
+    AppController,
+    MonstersController,
+    ItemsController,
+    WeaponsController,
+  ],
   providers: [AppService],
 })
 export class AppModule {}
